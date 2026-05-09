94b3ccab4f3cf593e0a5202580f65624757dd2e5
marwan-tarek <marwantarek75@gmail.com>
2026-05-09 19:32:19 +0300

    last one isa

--- main.tf
+++ main.tf
@@ -2,19 +2,16 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-# 1. توليد الـ Private Key (ملف الـ .pem)
 resource "tls_private_key" "marwan_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# 2. إنشاء الـ Key Pair في AWS باستخدام الـ Public Key المولد
 resource "aws_key_pair" "generated_key" {
   key_name   = "marwan-automation-key"
   public_key = tls_private_key.marwan_key.public_key_openssh
 }
 
-# 3. حفظ الـ Private Key على جهازك عشان تقدر تدخل SSH
 resource "local_file" "ssh_key" {
   filename = "${path.module}/marwan-automation-key.pem"
   content  = tls_private_key.marwan_key.private_key_pem
@@ -57,7 +54,7 @@ resource "aws_security_group" "jenkins_sg" {
 # 5. EC2 Instance
 resource "aws_instance" "jenkins_master" {
   ami           = "ami-09040d770ffe2224f" # Ubuntu 22.04 LTS for us-east-2
-  instance_type = "t3.micro" # <--- غير دي لـ t3.micro عشان الـ Free Tier في Ohio
+  instance_type = "t3.micro" 
   key_name      = aws_key_pair.generated_key.key_name
 
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
